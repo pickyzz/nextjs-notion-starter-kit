a85dda5f0c34342f23947d0e154f7dee48ac22ba
pickyzz <pikcolokung@gmail.com>
2021-12-30 17:27:15 +0700

    some tweak

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   images: {
-    domains: ['pbs.twimg.com', 'notion.so/image', 'images.unsplash.com']
+    domains: ['pbs.twimg.com', 'notion.so', 'images.unsplash.com']
   },
   eslint: {
     ignoreDuringBuilds: true,
